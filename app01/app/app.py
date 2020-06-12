330bdd5edc13d432aaf02e4d867d6b70b8586d7f
GuiEdington <guilhermeedington@gmail.com>
2020-06-12 11:44:57 -0300

    docker-compose e comentario no app01

--- app01/app/app.py
+++ app01/app/app.py
@@ -3,10 +3,22 @@ import requests
 
 app = Flask(__name__)
 
+# Rota / (raiz)
+#
+# Apenas redireciona o browser para roda /index
 @app.route('/', methods=['GET','POST'])
 def root():
    return redirect(url_for('.index'))
 
+# Rota /index
+#
+# Quando o método for GET, exibe a página templates/index.html.
+#
+# Quando for POST recebe os campos do  formulário  e  cria  uma 
+# requisição POST  para  http://app02:5000/mail  com  os  dados
+# em  forma   de  JSON.  Após  receber  a  resposta  de  app02, 
+# redireciona para  a  rota /thanks passando  o  nome  dado  no 
+# formulário como argumento.
 @app.route("/index", methods=['GET','POST'])
 def index():
    if request.method == 'POST':
@@ -16,6 +28,12 @@ def index():
       return redirect(url_for('.thanks', nome = nome))
    return render_template('index.html')
 
+# Rota /thanks
+#
+# Retorna a página temlpates/thanks.html caso receba algum nome
+# como parâmetro na URL na requisição.
+# 
+# Caso não receba, redireciona para a rota /index 
 @app.route("/thanks", methods=['GET','POST'])
 def thanks():
    nome = request.args.get('nome')
